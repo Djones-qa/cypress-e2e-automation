22797c93306c4c57a2d27828c332f5a20c6a0725
Darrius <bluefu40@gmail.com>
2026-03-20 16:38:38 -0400

    fix: increase timeouts and add retries for CI stability

--- cypress/e2e/auth.cy.ts
+++ cypress/e2e/auth.cy.ts
@@ -13,19 +13,19 @@ describe('Authentication', () => {
 
   it('should show error for locked out user', () => {
     LoginPage.login('locked_out_user', 'secret_sauce');
-    LoginPage.getErrorMessage().should('contain', 'Sorry, this user has been locked out');
+    LoginPage.getErrorMessage().should('contain', 'Sorry, this user has been locked out', { timeout: 15000 });
   });
 
   it('should show error for wrong password', () => {
     LoginPage.login('standard_user', 'wrong_password');
     LoginPage.getErrorMessage()
-      .should('be.visible')
+      .should('be.visible', { timeout: 15000 })
       .and('contain', 'Username and password do not match any user in this service');
   });
 
   it('should show error for empty credentials', () => {
     LoginPage.clickLogin();
-    LoginPage.getErrorMessage().should('contain', 'Username is required');
+    LoginPage.getErrorMessage().should('contain', 'Username is required', { timeout: 15000 });
   });
 
   it('should show login page title', () => {

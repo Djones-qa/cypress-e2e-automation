a8348b8cb761fe854599eeef17cb284916425578
Darrius <bluefu40@gmail.com>
2026-03-20 17:06:08 -0400

    fix: add wait between tests to prevent rate limiting in CI

--- cypress/e2e/auth.cy.ts
+++ cypress/e2e/auth.cy.ts
@@ -2,32 +2,32 @@
 import InventoryPage from '../pages/InventoryPage';
 
 describe('Authentication', () => {
-  it('should login successfully with valid credentials', () => {
+  beforeEach(() => {
+    cy.wait(3000);
     LoginPage.visit();
+  });
+
+  it('should login successfully with valid credentials', () => {
     LoginPage.login('standard_user', 'secret_sauce');
     InventoryPage.assertOnInventoryPage();
   });
 
   it('should show error for locked out user', () => {
-    LoginPage.visit();
     LoginPage.login('locked_out_user', 'secret_sauce');
     LoginPage.getErrorMessage().should('contain', 'Sorry, this user has been locked out');
   });
 
   it('should show error for wrong password', () => {
-    LoginPage.visit();
     LoginPage.login('standard_user', 'wrong_password');
     LoginPage.getErrorMessage().should('be.visible');
   });
 
   it('should show error for empty credentials', () => {
-    LoginPage.visit();
     LoginPage.clickLogin();
     LoginPage.getErrorMessage().should('contain', 'Username is required');
   });
 
   it('should show login page title', () => {
-    LoginPage.visit();
     cy.title().should('eq', 'Swag Labs');
   });
 });
